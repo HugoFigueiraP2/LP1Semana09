64ecfe1901fd47d9dc474fb2862b507cebab0343
Hugo Silva <hugofigueira.dev@gmail.com>
2024-05-09 17:07:33 +0100

    Adding Player script

---
+++ EqualPlayer/Player.cs
@@ -0,0 +1,24 @@
+namespace EqualPlayer
+{
+    public class Player
+    {   
+       
+        public PlayerClass      PClass { get; } 
+        public string           Name { get; }
+        public Player(PlayerClass pClass, string name)
+        {
+            PClass      = pClass;
+            Name        = name;
+        }
+        public override int GetHashCode()
+        {
+           return PClass.GetHashCode() ^ Name.GetHashCode(); 
+        }
+        public override bool Equals(object obj)
+        {
+            Player other = obj as Player;
+            if (other is null) return false; 
+            return Name == other.Name && PClass  == other.PClass;
+        }
+    }
+}
