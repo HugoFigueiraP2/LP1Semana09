e531eb08c8e9f629a775e6bbd4ecbc98e90954ad
Hugo Silva <hugofigueira.dev@gmail.com>
2024-05-09 17:27:28 +0100

    Adding FilePower2

--- FilePower2/Program.cs
+++ FilePower2/Program.cs
@@ -1,12 +1,45 @@
-﻿using System;
+﻿﻿using System;
+using System.IO;
 
 namespace FilePower2
 {
     class Program
     {
-        static void Main(string[] args)
+         private static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            string fileName; 
+
+            string line;
+
+
+            if (args.Length <= 0) 
+            {
+                Console.WriteLine("Must provide file name as argument");
+
+                return;
+            }
+
+            fileName = args[0];
+            Console.WriteLine("Enter text to add to file. Press enter to stop");
+
+            {           
+                using StreamWriter writer = new StreamWriter(fileName);
+
+                while(true)
+                {
+                    
+                    line = Console.ReadLine();
+
+                    
+                    if(line.Length <= 0) break;
+
+                   
+                    writer.WriteLine(line);
+                }
+
+
+            }
+
         }
     }
 }
