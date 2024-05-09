6140da3e23771805f0031cab952c61b936ec8b52
Hugo Silva <hugofigueira.dev@gmail.com>
2024-05-09 17:25:50 +0100

    Adding FilePower1

--- FilePower1/Program.cs
+++ FilePower1/Program.cs
@@ -1,12 +1,41 @@
-﻿using System;
+﻿﻿using System;
+using System.Collections.Generic;
+using System.IO;
 
 namespace FilePower1
 {
     class Program
     {
-        static void Main(string[] args)
+        private static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            string fileName; 
+            string line;
+
+            Queue<string> queue = new Queue<string>();
+
+            if (args.Length <= 0)
+            {
+                Console.WriteLine("Must provide file name as argument");
+
+                return;
+            }
+
+            fileName = args[0];
+
+
+            Console.WriteLine("Enter text to add to file. Press enter to stop");
+
+            while(true)
+            {
+                line = Console.ReadLine();
+
+                if(line.Length <= 0) break;
+
+                queue.Enqueue(line);
+            }
+
+            File.WriteAllLines(fileName, queue);
+
         }
     }
 }
