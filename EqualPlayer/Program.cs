3ea502880b988255ebc4f2f0e00a832cee64369d
Hugo Silva <hugofigueira.dev@gmail.com>
2024-05-09 17:24:13 +0100

    Adding program

--- EqualPlayer/Program.cs
+++ EqualPlayer/Program.cs
@@ -1,12 +1,39 @@
-﻿using System;
+﻿﻿using System;
+using System.Collections.Generic;
 
 namespace EqualPlayer
 {
     class Program
     {
-        static void Main(string[] args)
+        private static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Player player1 = new Player(PlayerClass.Tank, "Ana");
+
+            HashSet<Player> players = new HashSet<Player> 
+            { 
+                new Player(PlayerClass.Tank,        "Ana"), 
+                new Player(PlayerClass.Slayer,      "Paulo"),
+                new Player(PlayerClass.Tank,        "Ana"), 
+                
+
+                new Player(PlayerClass.Marksman,    "John"),
+                new Player(PlayerClass.Controller,  "Mary"),
+                new Player(PlayerClass.Slayer,      "Paulo"),
+                new Player(PlayerClass.Fighter,     "Lucas"),
+            };
+
+            foreach (Player player in players)
+            {
+                Console.Write($"{player.Name} is a {player.PClass} | Hash: ");
+
+                // Test of hash code
+                Console.Write(player.GetHashCode() + " | " );
+
+
+                // Test of Equals
+                Console.WriteLine(player.Equals(player1));
+         
+            }
         }
     }
 }
